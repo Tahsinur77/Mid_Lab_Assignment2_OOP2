3560516ddd46b59aa6fff326505920b88ae29df9
Md. Tahsinur Rahman Habib <Tahsinur777@users.noreply.github.com>
2021-03-05 01:34:19 +0600

    Main class part1

--- Mid_Lab_Assignment2/Mid_Lab_Assignment2/Program.cs
+++ Mid_Lab_Assignment2/Mid_Lab_Assignment2/Program.cs
@@ -10,6 +10,27 @@ namespace Mid_Lab_Assignment2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To our Bank");
+            Console.WriteLine("Type to select :");
+            Console.WriteLine("open => Open a Bank account\naccount => Perform transactions on an account\nquit => Exit the application");
+            string type = Console.ReadLine();
+            switch (type)
+            {
+                case "open":
+                    Console.WriteLine("Command For selecting Operation:");
+                    Console.WriteLine("savings => Open a savings account\nchecking => Open a checking account\nquit => Exit the application");
+
+                    break;
+                case "account":
+
+                    break;
+                case "quit":
+
+                    break;
+                default:
+                    break;
+
+            }
         }
     }
 }
