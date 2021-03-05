4eb95d31ca547ea8e4aad6a70e76a90573432c57
Md. Tahsinur Rahman Habib <Tahsinur777@users.noreply.github.com>
2021-03-05 11:37:16 +0600

    part 2.5
    change if else to switch case

--- Mid_Lab_Assignment2/Mid_Lab_Assignment2/Program.cs
+++ Mid_Lab_Assignment2/Mid_Lab_Assignment2/Program.cs
@@ -12,72 +12,74 @@ namespace Mid_Lab_Assignment2
         {
             Console.WriteLine("Welcome To our Bank");
             Bank bank = new Bank();
-            int check = 0;
+            bool check = false;
             while (true)
             {
                 Console.WriteLine("Type to select :");
                 Console.WriteLine("open => Open a Bank account\naccount => Perform transactions on an account\nquit => Exit the application");
                 string type = Console.ReadLine();
+                
                 switch (type)
-                {
+                { 
                     case "open":
+
+                        bool version1 = false;
                         while (true)
                         {
                             Console.WriteLine("Command For selecting Operation:");
                             Console.WriteLine("savings => Open a savings account\nchecking => Open a checking account\nquit => Exit the application");
                             string one = Console.ReadLine();
-                            if (one == "savings")
+                            switch(one)
                             {
-                                Console.Write("\t\t||Enter Account : \n\t\t||\tName : ");
-                                string accountName = Console.ReadLine();
-                                Console.Write("\t\t||\tDate of Birth : ");
-                                string dateOfBirth = Console.ReadLine();
-                                Console.Write("\t\t||\tBalance : ");
-                                double balance = Convert.ToDouble(Console.ReadLine());
-                                Console.Write("\t\t||\tAddress :\n\t\t||\t\tRoad No. : ");
-                                string roadNo = Console.ReadLine();
-                                Console.Write("\t\t||\t\tHouse No. : ");
-                                string houseNo = Console.ReadLine();
-                                Console.Write("\t\t||\t\tCity : ");
-                                string city = Console.ReadLine();
-                                Console.Write("\t\t||\t\tCountry : ");
-                                string country = Console.ReadLine();
-                                Account acc;
-                                acc = new SavingAccount(accountName, dateOfBirth, balance, new Address(roadNo, houseNo, city, country));
-                                bank.AddAccount(acc);
+                                case "savings":
+                                    Console.Write("\t\t||Enter Account : \n\t\t||\tName : ");
+                                    string accountName1 = Console.ReadLine();
+                                    Console.Write("\t\t||\tDate of Birth : ");
+                                    string dateOfBirth1 = Console.ReadLine();
+                                    Console.Write("\t\t||\tBalance : ");
+                                    double balance1 = Convert.ToDouble(Console.ReadLine());
+                                    Console.Write("\t\t||\tAddress :\n\t\t||\t\tRoad No. : ");
+                                    string roadNo1 = Console.ReadLine();
+                                    Console.Write("\t\t||\t\tHouse No. : ");
+                                    string houseNo1 = Console.ReadLine();
+                                    Console.Write("\t\t||\t\tCity : ");
+                                    string city1 = Console.ReadLine();
+                                    Console.Write("\t\t||\t\tCountry : ");
+                                    string country1 = Console.ReadLine();
+                                    Account acc1;
+                                    acc1 = new SavingAccount(accountName1, dateOfBirth1, balance1, new Address(roadNo1, houseNo1, city1, country1));
+                                    bank.AddAccount(acc1);
+                                    break;
+                                case "checking":
+                                    Console.Write("\t\t||Enter Account : \n\t\t||\tName : ");
+                                    string accountName2 = Console.ReadLine();
+                                    Console.Write("\t\t||\tDate of Birth : ");
+                                    string dateOfBirth2 = Console.ReadLine();
+                                    Console.Write("\t\t||\tBalance : ");
+                                    double balance2 = Convert.ToDouble(Console.ReadLine());
+                                    Console.Write("\t\t||\tAddress :\n\t\t||\t\tRoad No. : ");
+                                    string roadNo2 = Console.ReadLine();
+                                    Console.Write("\t\t||\t\tHouse No. : ");
+                                    string houseNo2 = Console.ReadLine();
+                                    Console.Write("\t\t||\t\tCity : ");
+                                    string city2 = Console.ReadLine();
+                                    Console.Write("\t\t||\t\tCountry : ");
+                                    string country2 = Console.ReadLine();
+                                    Account acc2;
+                                    acc2 = new CheckingAccount(accountName2, dateOfBirth2, balance2, new Address(roadNo2, houseNo2, city2, country2));
+                                    bank.AddAccount(acc2);
+                                    break;
+                                case "quit":
+                                    version1 = true;
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid Input...");
+                                    break;
                             }
-                            else if (one == "checking")
-                            {
-                                Console.Write("\t\t||Enter Account : \n\t\t||\tName : ");
-                                string accountName = Console.ReadLine();
-                                Console.Write("\t\t||\tDate of Birth : ");
-                                string dateOfBirth = Console.ReadLine();
-                                Console.Write("\t\t||\tBalance : ");
-                                double balance = Convert.ToDouble(Console.ReadLine());
-                                Console.Write("\t\t||\tAddress :\n\t\t||\t\tRoad No. : ");
-                                string roadNo = Console.ReadLine();
-                                Console.Write("\t\t||\t\tHouse No. : ");
-                                string houseNo = Console.ReadLine();
-                                Console.Write("\t\t||\t\tCity : ");
-                                string city = Console.ReadLine();
-                                Console.Write("\t\t||\t\tCountry : ");
-                                string country = Console.ReadLine();
-                                Account acc;
-                                acc = new CheckingAccount(accountName, dateOfBirth, balance, new Address(roadNo, houseNo, city, country));
-                                bank.AddAccount(acc);
-
-                            }
-                            else if (one == "quit")
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid Input...");
-                            }
+                            if (version1) break;
                             
                         }
-                        goto case "quit";
+                        break;
                     case "account":
                         while (true)
                         {
@@ -126,7 +128,7 @@ namespace Mid_Lab_Assignment2
                                         }
                                         else
                                         {
-                                            Console.WriteLine("\t\t||\tInvalid Input......");
+                                            Console.WriteLine("\t\t||\tInvalid Receiver Account Number......");
                                             
                                         }
                                     }
@@ -148,13 +150,13 @@ namespace Mid_Lab_Assignment2
                         }
                         goto case "quit";
                     case "quit":
-                        check = 1;
+                        check  = true;
                         break;
                     default:
                         break;
 
                 }
-                if (check == 1) break;
+                if (check) break;
             }
         }
     }
